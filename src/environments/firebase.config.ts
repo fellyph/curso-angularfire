67ea25f746c052ba9e69cdd46107981ad0f89ffb
Fellyph Cintra <fellyph.cintra@gmail.com>
2018-08-15 16:01:01 +0100

    Delete firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const configFirebase = {
-  apiKey: 'AIzaSyDNqNblNxugKIFBI_CQlcOzKyOxDn841nU',
-  authDomain: 'cursos-imedia.firebaseapp.com',
-  databaseURL: 'https://cursos-imedia.firebaseio.com',
-  projectId: 'cursos-imedia',
-  storageBucket: 'cursos-imedia.appspot.com',
-  messagingSenderId: '17240658739'
-};
